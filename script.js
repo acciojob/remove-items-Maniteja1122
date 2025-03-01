0788f920f4cb80ffb8259d60f523019280015172
MANITEJA KUNTA <129951439+Maniteja1122@users.noreply.github.com>
2025-03-01 11:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let selectid = document.getElementById("colorSelect");
+let btn = document.getElementsByTagName("input")[0];
+ btn.addEventListener("click",()=>{
+	 
+	 
+	  if(selectid.selectedIndex !== -1){
+		  selectid.remove(selectid.selectedIndex)
+	  }
+	
+ })
